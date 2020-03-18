acd03caebda5591991f431fbe58ab0c3970a179d
user <user@LG>
2020-03-18 23:07:17 +0900

    Create a menu

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+   public static void main(String[] args) {
+	   System.out.println("*** Inventory Management System Menu ***");
+	   System.out.println(" 1. Add Inventory");
+	   System.out.println(" 2. Delete Inventory"); 
+	   System.out.println(" 3. Edit Inventory");
+	   System.out.println(" 4. View Inventory");
+	   System.out.println(" 5. Exit");
+	   System.out.println("Select one number between 1 - 5:");
+   }  
+ }
